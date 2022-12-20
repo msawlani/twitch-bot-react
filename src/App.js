3dc701f75ed85dc43ab6033f71ca4f48957b4289
Michael <39811434+msawlani@users.noreply.github.com>
2022-12-20 12:01:23 -0500

    added bootstrap
    added bootstrap to style chat

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { client } from "./bot";
 import BadWords from "./data/badwords.json";
 import Commands from "./data/commands.json";
 import TimedMessages from "./data/TimedMessages.json";
+import "./App.css";
 
 const App = () => {
   const [messages, setMessages] = useState([]);
@@ -11,13 +12,18 @@ const App = () => {
     let username = userState.username;
     let newMessage = { username, message };
     setMessages([...messages, newMessage]);
+    console.log(userState);
   });
 
   return (
-    <div className="App">
-      {messages.map((message) => (
-        <li>{`${message.username} : ${message.message}`}</li>
-      ))}
+    <div className="fluid-container bg-black text-white">
+      <div className="m-5">
+        <ul className="list-unstyled">
+          {messages.map((message, id) => (
+            <li key={id}>{`${message.username} : ${message.message}`}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
