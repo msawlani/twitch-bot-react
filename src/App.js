b32c44149ece4724cf7535a3284a75277b6c661c
Michael <39811434+msawlani@users.noreply.github.com>
2023-01-07 20:00:29 -0500

    sidenav
    added links to sidenav that change the content on the page.
    
    added login button and placeholder for twitch picture

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Chat from "./Chat";
+import Commands from "./Commands";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route path="chat" element={<Chat />}></Route>
+          <Route index element={<Chat />} />
+          <Route path="commands" element={<Commands />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,16 +1,34 @@
 import React from "react";
+import links from "./data/NavLinks.json";
+import { NavLink } from "react-router-dom";
+import "./SideBar.css";
 
 const NavBar = () => {
   return (
     <div className="vh-100 bg-secondary w-25">
-      <div className="">
-        <h4>user profile</h4>
-        <a
-          href="https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=rgllofa4q2yeh1z61l1xt76boajucs&redirect_uri=http://localhost:3000/&scope=user_read"
-          target="_blank"
-        >
-          Login
-        </a>
+      <div className="text-center">
+        <span>Picture</span>
+
+        <ul className=" list-unstyled">
+          {links.Links.map((link, id) => (
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? "active" : "main-nav")}
+                to={link.link}
+              >
+                {link.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <div className="text-center">
+          <a
+            href="https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=rgllofa4q2yeh1z61l1xt76boajucs&redirect_uri=http://localhost:3000/&scope=user_read"
+            target="_blank"
+          >
+            Login
+          </a>
+        </div>
       </div>
     </div>
   );
