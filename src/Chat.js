21086af8671b86a5f1f6d15cbc1afc046b1b6383
Michael <39811434+msawlani@users.noreply.github.com>
2023-08-02 22:00:23 -0400

    Update Chat.js
    fixed sizing of chat and player

--- src/Chat.js
+++ src/Chat.js
@@ -22,51 +22,49 @@ const Chat = ({ chat, setChat, userData }) => {
   };
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 col-md-9">
-          <iframe
-            src="https://player.twitch.tv/?channel=sinsofaninja&parent=ninjashideout-twitch-bot.onrender.com&autoplay=true&muted=true"
-            height="1000"
-            width="850"
-            frameborder="0"
-            scrolling="no"
-            allowfullscreen="true"
-          ></iframe>
-        </div>
+    <div className="row">
+      <div className="col-12 col-md-8">
+        <iframe
+          src={`https://player.twitch.tv/?channel=sinsofaninja&parent=${process.env.REACT_APP_PLAYERURL}&autoplay=true&muted=true`}
+          height="1000"
+          width="1350"
+          frameborder="0"
+          scrolling="no"
+          allowfullscreen="true"
+        ></iframe>
+      </div>
 
-        <div className="chatWindow col-12 col-md-3">
-          {chat.map((msg, index) => (
-            <div key={index}>
-              <strong style={{ color: msg.color }}>{msg.user}: </strong>
-              <span>{msg.text}</span>
-            </div>
-          ))}
-          <form onSubmit={sendMessage} hidden={client.readyState() !== "OPEN"}>
-            <div className="form-group">
-              <input
-                className="form-control form-control-sm"
-                type="text"
-                placeholder="Type a message"
-                value={messages}
-                onChange={(e) => setMessages(e.target.value)}
-              />
-            </div>
+      <div className="chatWindow col-12 col-md-4">
+        {chat.map((msg, index) => (
+          <div key={index}>
+            <strong style={{ color: msg.color }}>{msg.user}: </strong>
+            <span>{msg.text}</span>
+          </div>
+        ))}
+        <form onSubmit={sendMessage} hidden={client.readyState() !== "OPEN"}>
+          <div className="form-group">
+            <input
+              className="form-control form-control-sm"
+              type="text"
+              placeholder="Type a message"
+              value={messages}
+              onChange={(e) => setMessages(e.target.value)}
+            />
+          </div>
 
-            <div className="d-flex justify-content-center">
-              <button className="btn btn-primary" type="submit">
-                Send
-              </button>
-              <button
-                className="btn btn-danger"
-                onClick={clearChat}
-                type="button"
-              >
-                Clear
-              </button>
-            </div>
-          </form>
-        </div>
+          <div className="d-flex justify-content-center">
+            <button className="btn btn-primary" type="submit">
+              Send
+            </button>
+            <button
+              className="btn btn-danger"
+              onClick={clearChat}
+              type="button"
+            >
+              Clear
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
